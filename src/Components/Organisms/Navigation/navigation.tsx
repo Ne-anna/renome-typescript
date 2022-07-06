8cdbf25383a549c3baf5b1cd154c71de0db56b00
Ne-anna <annabusele@inbox.lv>
2022-07-06 16:32:09 +0300

    Add hooks and props

--- src/Components/Organisms/Navigation/navigation.tsx
+++ src/Components/Organisms/Navigation/navigation.tsx
@@ -1,27 +1,46 @@
 import "../Navigation/navigation.scss";
-import { NavigationData, Cart, MenuData } from "../../../data";
+import { NavigationData, Cart, MenuData, backButtonData } from "../../../data";
 import Haburger from "../../Attoms/Hamburger-button/hamburger";
 import CartDropDown from "../../Molecules/cart-drop-down/cart-drop-down";
 import Menu from "../../Molecules/menu/menu";
+import SubMenu from "../../Molecules/sub-menu/sub-menu";
 import { useRef, useState } from "react";
 
-const Navigation = (props: NavigationData & Cart & MenuData) => {
+const Navigation = (
+  props: NavigationData & Cart & MenuData & backButtonData
+) => {
   const [isCartOpen, setCartOpen] = useState(false);
   const [isMainMenuOpen, setMainMenuOpen] = useState(false);
+  const [isSubMenuOpen, setSubMenuOpen] = useState(false);
 
   const Ref = useRef<HTMLDivElement>(null);
 
   const toggleCart = () => {
     setMainMenuOpen(false);
     setCartOpen(!isCartOpen);
+    setSubMenuOpen(false);
   };
 
   const toggleMenu = () => {
     setMainMenuOpen(!isMainMenuOpen);
     setCartOpen(false);
+    if (isSubMenuOpen) {
+      setMainMenuOpen(false);
+      setSubMenuOpen(false);
+    }
   };
 
-  if (isMainMenuOpen || isCartOpen) {
+  const openSubMenu = () => {
+    setSubMenuOpen(true);
+    setMainMenuOpen(false);
+  };
+
+  const closeSubMenu = () => {
+    setSubMenuOpen(false);
+    setMainMenuOpen(true);
+  };
+
+  if (isMainMenuOpen || isSubMenuOpen || isCartOpen) {
     document.body.style.overflow = "hidden";
   } else {
     document.body.style.overflow = "unset";
@@ -44,13 +63,24 @@ const Navigation = (props: NavigationData & Cart & MenuData) => {
           alt={props.altTagDivider}
         />
         <div
-          className={`navigation__hamburger${isMainMenuOpen ? " active" : ""}`}
+          className={`navigation__hamburger${
+            isMainMenuOpen ? " active" : isSubMenuOpen ? " active" : ""
+          }`}
           onClick={toggleMenu}
         >
           <Haburger />
         </div>
         {isCartOpen && <CartDropDown cart={props.cart} />}
-        {isMainMenuOpen && <Menu menu={props.menu} />}
+        {isMainMenuOpen && <Menu menu={props.menu} openSubMenu={openSubMenu} />}
+        {isSubMenuOpen && (
+          <SubMenu
+            menu={props.menu}
+            closeSubMenu={closeSubMenu}
+            title={props.buttonData.title}
+            buttonBackPath={props.buttonData.buttonBackPath}
+            buttonBackAltTag={props.buttonData.buttonBackAltTag}
+          />
+        )}
       </div>
     </nav>
   );
