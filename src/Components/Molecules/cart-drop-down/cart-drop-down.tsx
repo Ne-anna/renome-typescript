dc6adab39290754ed3796741ed0a19afae6fabcd
Ne-anna <annabusele@inbox.lv>
2022-07-06 10:42:52 +0300

    Display cartItem item with map

--- src/Components/Molecules/cart-drop-down/cart-drop-down.tsx
+++ src/Components/Molecules/cart-drop-down/cart-drop-down.tsx
@@ -1,7 +1,18 @@
 import "../cart-drop-down/cart-drop-down.scss";
+import { Cart } from "../../../data";
 
-const CartDropDown = () => {
-  return <div className="cart">make a fish</div>;
+const CartDropDown = (props: Cart) => {
+  return (
+    <div className="cart">
+      {props.cart.map((post, index) => {
+        return (
+          <div className="cart__item" key={index}>
+            <p className="cart__text">{post.item}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CartDropDown;
