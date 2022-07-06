54a7f515cbf7ed1cb32dd9a332e7321d58d60bba
Ne-anna <annabusele@inbox.lv>
2022-07-06 13:11:40 +0300

    Create buttons - search and next for menu.tsx

---
+++ src/Components/Attoms/search-button/search-button.tsx
@@ -0,0 +1,12 @@
+import { MenuItem } from "../../../data";
+import "../search-button/search-button.scss";
+
+const Search = (props: MenuItem) => {
+  return (
+    <button className="navigation__button--search">
+      <img src={props.buttonSearchPath} alt={props.searchAltTag} />
+    </button>
+  );
+};
+
+export default Search;
