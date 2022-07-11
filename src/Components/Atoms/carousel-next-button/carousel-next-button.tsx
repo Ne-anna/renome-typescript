81a71e6e30e536188862dbfbb3bdb95a8fb0cca4
Ne-anna <annabusele@inbox.lv>
2022-07-11 16:03:30 +0300

    Add data-testId

--- src/Components/Atoms/carousel-next-button/carousel-next-button.tsx
+++ src/Components/Atoms/carousel-next-button/carousel-next-button.tsx
@@ -3,7 +3,11 @@ import "../carousel-next-button/carousel-next-button.scss";
 
 const CarouselNextButton = (props: CarouselButton) => {
   return (
-    <button className="carousel__button" onClick={props.loadNextSlide}>
+    <button
+      className="carousel__button"
+      onClick={props.loadNextSlide}
+      data-testid="left-button"
+    >
       <img src={props.carouselButtonNext} alt={props.altTagNext} />
     </button>
   );
